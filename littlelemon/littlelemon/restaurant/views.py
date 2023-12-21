2c7e2504b2f68ee5614eaf543cd7ed027c1c22a6
Santiago <danielsantiago1230@gmail.com>
2023-12-20 21:49:57 -0500

    djoser and authentication

--- littlelemon/littlelemon/restaurant/views.py
+++ littlelemon/littlelemon/restaurant/views.py
@@ -1,8 +1,10 @@
 from django.shortcuts import render
 from rest_framework.generics import ListCreateAPIView, RetrieveUpdateAPIView, DestroyAPIView
 from rest_framework.viewsets import ModelViewSet
+from rest_framework.permissions import IsAuthenticated
 from .serializers import MenuSerializer, BookingSerializer
 from .models import Menu, Booking
+
 
 # Create your views here.
 def index(request):
@@ -17,6 +19,7 @@ class SingleMenuItemView(RetrieveUpdateAPIView, DestroyAPIView):
     queryset = Menu.objects.all()
 
 class BookingViewSet(ModelViewSet):
+    permission_classes = [IsAuthenticated]
     serializer_class = BookingSerializer
     queryset = Booking.objects.all()
 
